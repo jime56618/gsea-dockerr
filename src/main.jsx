d5d4a2d6936e41d9716f1c87618cb3c5b0413258
jime56618 <jimena56618@gmail.com>
2026-07-09 01:53:56 +0000

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // <-- 1. Agregamos esta importación
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 import App from './App.jsx'
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, null, redirect);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* <-- 2. Envolvemos la App */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
